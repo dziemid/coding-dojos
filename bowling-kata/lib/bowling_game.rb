c9ad1499f4dec505e92540d2d542e845aa8a8eb7
Greg Dziemidowicz <greg@cogent.co>
2014-03-25 20:56:19 +1100

    rewritten with approach of remembering all the frames

--- bowling-kata/lib/bowling_game.rb
+++ bowling-kata/lib/bowling_game.rb
@@ -17,16 +17,20 @@ class BowlingGame
   end
 
   def score
-    return @score unless @score == 40
     sum = 0
     while not @all_frames.empty?
       current = @all_frames.shift
+      if current.spare?
+        sum+=@all_frames[0].first_roll
+      end
       if current.strike?
         next_frame = @all_frames[0]
-        if next_frame.strike?
-          sum+=10+@all_frames[1].first_roll
-        else
-          sum+=next_frame.rolls.inject(:+)
+        if next_frame
+          if next_frame.strike?
+            sum+=10+@all_frames[1].first_roll
+          else
+            sum+=next_frame.rolls.inject(:+)
+          end
         end
       end
       sum+=current.rolls.inject(:+)
