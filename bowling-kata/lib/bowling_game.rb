2b473f7fecf7d7fcd0ce0000be9c4f9b24e51f98
Greg Dziemidowicz <greg@cogent.co>
2014-03-25 20:09:39 +1100

    Keeping track of all the rolls

--- bowling-kata/lib/bowling_game.rb
+++ bowling-kata/lib/bowling_game.rb
@@ -6,9 +6,11 @@ class BowlingGame
   def initialize
     @score = 0
     @prev_state = NothingSpecial.new
+    @all_rolls = []
   end
 
   def roll_frame rolls
+    @all_rolls += rolls
     frame = Frame.new(rolls)
     @score += frame.score_based_on(@prev_state)
     @prev_state = frame.state
